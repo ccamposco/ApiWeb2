3774ea982cbe1b08529051c77ad82a99c8ec4d5b
cesarcarlosc <fccampos@idat.edu.pe>
2021-11-26 10:23:45 -0500

    mi primer commit

---
+++ Data/ApiWeb2Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ApiWeb2.Models;
+
+namespace ApiWeb2.Data
+{
+    public class ApiWeb2Context : DbContext
+    {
+        public ApiWeb2Context (DbContextOptions<ApiWeb2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Persona> Persona { get; set; }
+    }
+}
---
+++ Models/Persona.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiWeb2.Models
+{
+    public class Persona
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Sexo { get; set; }
+        public string Dni { get; set; }
+    }
+}
